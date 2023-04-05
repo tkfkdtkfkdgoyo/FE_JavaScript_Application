43422bbb79aa20e505662e3d7b3d261ade88a78b
makeum <jo_god_da@inha.edu>
2023-04-05 20:47:47 +0900

    찐최종

--- Problem1.js
+++ Problem1.js
@@ -6,7 +6,7 @@ let Name = "나현";
 
 alert(`hello ${1}`); // hello 1
 
-alert(`hello ${"Name"}`); // hello Name .... ? 
+alert(`hello ${"Name"}`); // hello Name
 
 alert("hello ${Name}"); // hello ${name}
 
